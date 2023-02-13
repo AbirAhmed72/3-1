47bfb9586ffb13c6739ba88802a9c291940860b5
AbirAhmed72 <abirahmed14872@gmail.com>
2023-02-13 10:48:52 +0600

    php files included

---
+++ Web Tech/PHP/form.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+        <!-- <h1>
+           <?php
+                // echo "first php code";
+            ?> 
+        </h1>
+
+        <p>
+            <?php
+                // $cgpa = array("Abir"=>3.5,
+                //             "Sharif"=>3.7,
+                //             "Fahad"=>4.3);
+                // $name = "Abir";
+                // echo "this is " , $name, "'s CGPA: ", $cgpa[$name];
+            ?>
+        </p> -->
+
+
+        <!-- <form action="sub.php" method="post">
+
+            Name: <input type="text" name="myName"><br>
+            Email: <input type="text" name="myEmail"><br>
+            <input type="submit">
+        </form> -->
+
+        <form action="sub.php" method="post">
+
+            NID: <input type="text" name="myNID"><br>
+            Birthday:<input type="date" name="myBirthday"><br>
+            J2KM7O: <input type="text" name="myCap"><br>
+            <!-- Email: <input type="text" name="myEmail"><br> -->
+            <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ Web Tech/PHP/sub.php
@@ -0,0 +1,27 @@
+<?php
+    // echo $_POST["myName"];
+    // echo $_POST["myEmail"];
+?>
+
+<html>
+
+<body>
+    <ul>
+        <li>
+            <?php
+                echo $_POST["myNID"];
+            ?>
+        </li>
+        <li>
+            <?php
+                echo $_POST["myBirthday"];
+            ?>
+        </li>
+        <li>
+            <?php
+                echo $_POST["myCap"];
+            ?>
+        </li>
+    </ul>
+</body>
+</html>
