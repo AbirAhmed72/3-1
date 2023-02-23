b16b831c84807db4c39e9085d72066b4465952bc
AbirAhmed72 <abirahmed14872@gmail.com>
2023-02-23 23:11:49 +0600

    trying react

---
+++ Tutorials/react-nerds-lesson/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+	return <h1> This is Abir trying react </h1>
+}
+function App() {								//root
+	return <Header/>;
+}
+
+export default App;
