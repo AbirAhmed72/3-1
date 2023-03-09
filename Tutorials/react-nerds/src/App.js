368d8d5fb59fb820f0cadea067447b184b2f44e9
AbirAhmed72 <abirahmed14872@gmail.com>
2023-03-09 15:00:12 +0600

    before react forms

--- Tutorials/react-nerds/src/App.js
+++ Tutorials/react-nerds/src/App.js
@@ -8,34 +8,69 @@ import Child from './components/Child';
 import Header from './components/Header';
 import {useReducer} from 'react';
 
+import Homepage from './components/Homepage';
+import AboutMe from './components/AboutMe';
+import {Routes, Route, Link} from 'react-router-dom';
+
+const topDesserts = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "The best tiramisu in town",
+    image: "https://picsum.photos/20Ø/3ØØ/?random" ,
+    price: "$5.00",
+    
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description: "Mind blowing taste",
+    image: "https://picsum.photos/20Ø/3ØØ/?random" ,
+    price: "$4.50",
+    
+  },
+  {
+    id: "3",
+    title: "Chocolate mousse",
+    description: "Unexplored flavour",
+    image: "https://picsum.photos/20Ø/3ØØ/?random" ,
+    price: "$6.50",
+    
+  }
+];
+
 // function Header(){
 // 	return <h1> This is Abir trying react </h1>
 // }
 
-const reducer = (state, action) => {
-	if(action.type === 'Buy') return {games: state.games + 1};
-	if(action.type === 'Sell') return {games: state.games - 1};
-}
+// const reducer = (state, action) => {
+// 	if(action.type === 'Buy') return {games: state.games + 1};
+// 	if(action.type === 'Sell') return {games: state.games - 1};
+// }
 
 function App() {								//root
 	
-	//================useState=======================================
-	const [Word, setWord] = React.useState("Using stateful thing!!");
-	const [Khabar, setKhabar] = useState("BHAAT");
+	// //================useState=======================================
+	// const [Word, setWord] = React.useState("Using stateful thing!!");
+	// const [Khabar, setKhabar] = useState("BHAAT");
 
-	function handleClick(){
-		Khabar = "BHAAT" ? setKhabar("BIRIYANI") : setKhabar("BHAAT");
-	}
-	//================useState=======================================
+	// function handleClick(){
+	// 	Khabar = "BHAAT" ? setKhabar("BIRIYANI") : setKhabar("BHAAT");
+	// }
+	// //================useState=======================================
 
-	//================useReducer=======================================
-	const initialGames = {games: 5};
-	const [state, transaction] = useReducer(reducer, initialGames);
-	//================useReducer=======================================
+	// //================useReducer=======================================
+	// const initialGames = {games: 5};
+	// const [state, transaction] = useReducer(reducer, initialGames);
+	// //================useReducer=======================================
 		
+    const listItems = topDesserts.map(dessert => {
+      const itemText = `${dessert.title} - ${dessert.price}`
+      return <li>{itemText}</li>
+    })
 	return (	
 	<div>
-		<BtnClk/>
+		{/* <BtnClk/>
 		<BtnHov/>
 	 	<ModeToggler/>
 		<Child message = {"Message has been passed through child component"} />
@@ -49,7 +84,22 @@ function App() {								//root
 				<button onClick={() => transaction({type: 'Buy'})}>Buy Games</button>
 				<button onClick={() => transaction({type: 'Sell'})}>Sell Games</button>
 			</div>
-		</div>
+		</div> */}
+    {/* <div className='App'>
+      <nav className='nav'>
+        <Link to='/' className ='nav-item'>Homepage</Link>
+        <Link to='/about-me' className ='nav-item'>About Me</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/about-me' element={<AboutMe/>}/>
+      </Routes>
+    </div> */}
+    <div>
+      <ol>
+        {listItems}
+      </ol>
+    </div>
 	</div>
 
 	
